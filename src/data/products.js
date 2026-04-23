75c57eb260a3609b6f0b6df6be6ebf73c85cf685
Fatiha-تعاونية النسيج الممتاز <essemlalimoulayhfid1973@gmail.com>
2026-04-24 00:05:30 +0100

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,10 @@
+export const products = [
+  {
+    id: 1,
+    name: "زربية أمزميز الملكية",
+    price: "حسب القياس",
+    image: "/images/tapis1.jpg",
+    description: "نسيج يدوي أصيل بصوف طبيعي 100%"
+  }
+];
+
