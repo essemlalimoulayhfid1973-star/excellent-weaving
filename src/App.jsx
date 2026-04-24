6921ef6c8f44ff2a825e5a871b7ddf0f257b51f3
Fatiha-تعاونية النسيج الممتاز <essemlalimoulayhfid1973@gmail.com>
2026-04-24 23:15:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
 import Home from './pages/Home';
-import AiChatBot from './components/AiChatBot';
-import './styles/global.css';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="app">
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </main>
-          <AiChatBot />
-        </div>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
