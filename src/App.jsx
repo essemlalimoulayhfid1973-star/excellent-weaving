519ca36e62fcc28d8162ca7d24e9a7a8a4445dc9
Fatiha-تعاونية النسيج الممتاز <essemlalimoulayhfid1973@gmail.com>
2026-04-25 00:35:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Home />} />
       </Routes>
     </Router>
   );
